4f806575e93f6af14876b79c1a4f9b19213d9ba6
Keely Hutchinson <hutchinsonkeely@ksu.edu>
2021-09-22 12:24:40 -0500

    updated unit tests and updated largest element function

--- first_largest.py
+++ first_largest.py
@@ -2,11 +2,16 @@
 Python program to find the largest element and its location.
 """
 
-def largest_element(a):
-    """ Return the largest element of a sequence a.
-    """
-
-    return None
+def largest_element(a, loc=True):
+    element = a[0]
+    maxloc = 0
+    for i in range(0,len(a)):
+        if a[i] > element:
+            element=a[i]
+            maxloc = i
+    if loc==True:
+        return element, maxloc
+    return element
 
 
 if __name__ == "__main__":
